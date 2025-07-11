855d484a84b69825be5eac762ea62ce0103615d6
ZehraOkurrr <zehra.okur@medianova.com>
2025-07-11 14:28:47 +0300

    databasede değişiklikler yapıldı.

--- app/database.py
+++ app/database.py
@@ -7,6 +7,8 @@ DB_PASSWORD = "password"
 DB_HOST = "mysql_db"  # docker-compose içindeki servis adı
 DB_NAME = "proje_db"
 
+
+DB_PORT = "3310"
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
