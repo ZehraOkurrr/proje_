5eb84cb32967af23b53978ef6ab3eaad568549c1
ZehraOkurrr <zehra.okur@medianova.com>
2025-07-11 11:49:14 +0300

    db myql_db olarak değiştirildi

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = "user"
 DB_PASSWORD = "password"
-DB_HOST = "db"  # docker-compose içindeki servis adı
+DB_HOST = "mysql_db"  # docker-compose içindeki servis adı
 DB_NAME = "proje_db"
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
