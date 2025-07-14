25930b50c88e9a17c32e2eb9e707562147852f78
ZehraOkurrr <zehra.okur@medianova.com>
2025-07-14 09:58:46 +0300

    sorunlar giderildi

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = "user"
 DB_PASSWORD = "password"
-DB_HOST = "mysql_db"  # docker-compose içindeki servis adı
+DB_HOST = "mysql_db"  # Docker içindeki servis adı
+DB_PORT = "3306"      # Docker içindeki MySQL portu
 DB_NAME = "proje_db"
 
-
-DB_PORT = "3310"
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
